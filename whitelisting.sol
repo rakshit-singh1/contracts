3e69679ad1dbc6b869c6b4159faaa15afbb8a1c0
rakshit-singh1 <143591250+rakshit-singh1@users.noreply.github.com>
2023-09-04 10:02:45 +0530

    Initializing repository

---
+++ whitelisting.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.5.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.5.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.5.0/utils/Counters.sol";
+import "@openzeppelin/contracts@4.5.0/utils/cryptography/MerkleProof.sol";
+
+contract MyToken is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    bytes32 public root;
+    uint non_member_price=0.1 ether;
+    Counters.Counter private _tokenIdCounter;
+
+    constructor(bytes32 _root) ERC721("MyToken", "MTK") {
+        root = _root;
+    }
+
+    function safeMint(address to, bytes32[] memory proof) public payable{
+        require(isValid(proof, keccak256(abi.encodePacked(msg.sender))), "Not a part of Allowlist. Therefore payment of 0.1 ether per mint required");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+    function paidMint(address to) public payable{
+        require(msg.value >= non_member_price, "Please pay the required price");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function isValid(bytes32[] memory proof, bytes32 leaf) public view returns (bool) {
+        return MerkleProof.verify(proof, root, leaf);
+    }
+}
