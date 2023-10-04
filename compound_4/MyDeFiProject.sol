4e40bd7ee3ab5e9b1fa026a716739def8c28c0b3
rakshit-singh1 <143591250+rakshit-singh1@users.noreply.github.com>
2023-10-04 14:13:25 +0530

    compound run

--- compound_4/MyDeFiProject.sol
+++ compound_4/MyDeFiProject.sol
@@ -3,26 +3,26 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./CTokenInterface.sol";
-import "./ComptrollerInterface.sol";
+// import "./ComptrollerInterface.sol";
 import "./PriceOracleInterface.sol";
 //import "./interestRateModel.sol";
 import "./LoanToken.sol";
 
 contract MyDeFiProject is Loan{
-    ComptrollerInterface public comptroller;
+    // ComptrollerInterface public comptroller;
     // PriceOracleInterface public priceOracle;
     IERC20 public token;
     CTokenInterface public cToken;
     //InterestRateModel public JumpRateModel; 
 
     constructor(
-        address _comptroller,
+        // address _comptroller,
         // address _priceOracle,
         address _token,
         address _cToken
         //address _JumpRateModel
     ) {
-        comptroller = ComptrollerInterface(_comptroller);
+        // comptroller = ComptrollerInterface(_comptroller);
         // priceOracle = PriceOracleInterface(_priceOracle);
         cToken = CTokenInterface(_cToken);
         token = IERC20(_token);
@@ -55,11 +55,14 @@ contract MyDeFiProject is Loan{
     function invest(uint underlyingAmount) public{
         // address underlyingAddress = address(token);
         // IERC20(underlyingAddress).approve(address(cToken), underlyingAmount);
+        require(token.approve(address(cToken), underlyingAmount), "Approval failed");
         uint result = cToken.mint(underlyingAmount);
         require(result == 0, "mint() failed");
         // UnderlyingPrice[currId()-1] = priceOracle.getUnderlyingPrice(address(cToken));
         safeMint(msg.sender);
         Liquidity[currId()-1] = cToken.balanceOf(address(this));
+        uint tokenToWithdraw = cToken.balanceOf(address(this));
+        tokenToWithdraw = cToken.balanceOf(address(this)) - tokenToWithdraw;
     }
     // function interestRate(uint id) public view returns (uint){
     //     return JumpRateModel.getSupplyRate(Liquidity[id], totalBorrows, cToken.balanceOf(address(this))-totalBorrows, cToken.reserveFactorMantissa());
@@ -80,27 +83,27 @@ contract MyDeFiProject is Loan{
         super._transfer(from,to,tokenId);
     }
 
-    function enterMarket() external {
-        address[] memory markets = new address[](1);
-        markets[0] = address(cToken);
-        uint[] memory results = comptroller.enterMarkets(markets);
-        require(results[0] == 0, "enterMarket() failed");
-    }
+    // function enterMarket() external {
+    //     address[] memory markets = new address[](1);
+    //     markets[0] = address(cToken);
+    //     uint[] memory results = comptroller.enterMarkets(markets);
+    //     require(results[0] == 0, "enterMarket() failed");
+    // }
 
-    function borrow(uint borrowAmount) external {
-        //address underlyingAddress = cToken.underlying();
-        totalBorrows+=borrowAmount;
-        uint result = cToken.borrow(borrowAmount);
-        require(result == 0, "borrow() failed");
-    }
+    // function borrow(uint borrowAmount) external {
+    //     //address underlyingAddress = cToken.underlying();
+    //     totalBorrows+=borrowAmount;
+    //     uint result = cToken.borrow(borrowAmount);
+    //     require(result == 0, "borrow() failed");
+    // }
 
-    function repayBorrow(uint underlyingAmount) external {
-        totalBorrows-=underlyingAmount;
-        address underlyingAddress = cToken.underlying();
-        IERC20(underlyingAddress).approve(address(cToken), underlyingAmount);
-        uint result = cToken.repayBorrow(underlyingAmount);
-        require(result == 0, "repayBorrow() failed");
-    }
+    // function repayBorrow(uint underlyingAmount) external {
+    //     totalBorrows-=underlyingAmount;
+    //     address underlyingAddress = cToken.underlying();
+    //     IERC20(underlyingAddress).approve(address(cToken), underlyingAmount);
+    //     uint result = cToken.repayBorrow(underlyingAmount);
+    //     require(result == 0, "repayBorrow() failed");
+    // }
 
     function transferEther(address payable receiver) public payable {
         require(msg.value > 0, "Please send some Ether");
@@ -126,4 +129,4 @@ contract MyDeFiProject is Loan{
 //0x5A74332C881Ea4844CcbD8458e0B6a9B04ddb716
 //0xd731C20d311839e829963831C6299B6dD8FA691a
 
-//contract address:0x1249cB13d9b9bf678b1d5a5cA9d9a9EfAc968B0E
+//contract address:0x0b81069209355c5060b9a52189D809531D53656f
